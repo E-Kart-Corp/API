a833731b8b664360b1bd4da5c55d48d3b9c51261
Brice HUET <brice.huet@epitech.eu>
2024-12-02 17:22:26 +0000

    add hello world

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.use("/api", productRoutes);
 
 const server = http.createServer(app);

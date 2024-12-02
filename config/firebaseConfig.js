cdbcb8313bcd769a6d45b19457f980fba64d0b84
Brice HUET <brice.huet@epitech.eu>
2024-12-02 17:32:06 +0000

    fix prod firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,16 +2,7 @@ require("dotenv").config();
 
 const firebaseAdmin = require("firebase-admin");
 
-// if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-//   throw new Error(
-//     "La variable d'environnement GOOGLE_APPLICATION_CREDENTIALS n'est pas définie."
-//   );
-// }
-
-// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 firebaseAdmin.initializeApp({
-  // credential: firebaseAdmin.credential.cert(serviceAccount),
   credential: firebaseAdmin.credential.cert({
     type: process.env.TYPE,
     project_id: process.env.PROJECT_ID,

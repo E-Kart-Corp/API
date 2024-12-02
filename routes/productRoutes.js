8904e9b67c5ea44a189fd2de7a35c70977246b9a
Brice HUET <brice.huet@epitech.eu>
2024-12-02 18:08:11 +0000

    fix route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,9 +8,9 @@ const {
 const upload = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
-router.post("/create_product", upload.array('image'), createProduct);
-router.post("/check_product", upload.single("image"), checkProduct);
-router.get("/getproduct", getProducts);
+router.post("/createProduct", upload.array('image'), createProduct);
+router.post("/checkProduct", upload.single("image"), checkProduct);
+router.get("/getProduct", getProducts);
 router.get("/getCategory", getCategory);
 
 
